12351edd27793588feb34a3cc6dfd48c0433bf57
ELshnoudy <mahmo7dmohamed555@gmail.com>
2025-06-20 00:24:32 +0300

    frist commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+var wibsiteNameInput= document.getElementById("bookmarkName")
+var wibsiteURLInput= document.getElementById("bookmarkURL")
+
+
+var websitesArr=[];
+if (localStorage.getItem("website")!=null) {
+    websitesArr = JSON.parse(localStorage.getItem("website")) 
+    displayWibsites();
+}
+
+function addWibsite() {
+
+    var Wibsite = {
+
+        name : wibsiteNameInput.value ,
+        URL : wibsiteURLInput.value,
+    }
+
+    websitesArr.push(Wibsite);
+    localStorage.setItem("website",JSON.stringify(websitesArr))
+    displayWibsites();
+    reset();
+}
+
+function displayWibsites() {
+    var cartoona = ""
+    for (var i =0 ; i < websitesArr.length ;i++) {
+        cartoona += `<tr>
+        <td> ${i+1}</td> 
+        <td>${websitesArr[i].name}</td> 
+        <td><button class = " btn btn-danger" onclick=visitWebsite(${i})>Vist</button></td>       
+        <td><button class = " btn btn-outline-danger" onclick=deleteWebsite(${i})>Delete</button></td>       
+        </tr>`
+    }
+    document.getElementById("tBody").innerHTML=cartoona ;
+}
+
+function visitWebsite(wibsiteIndex) {
+    window.location.href= websitesArr[wibsiteIndex].URL
+}
+
+function reset() {
+    wibsiteNameInput.value = "";
+    wibsiteURLInput.value = "";
+}
+
+function deleteWebsite(wibsiteIndex) {
+    websitesArr.splice(wibsiteIndex,1);
+    localStorage.setItem("website",JSON.stringify(websitesArr))
+    displayWibsites();
+}
